acdeaee26ccde23d02ab072ce9921f34abd91ee7
Peter Gil Villar <villarpeter@gmail.com>
2018-03-21 11:54:16 +0700

    Ongoing on implementing test project

--- XYZCorp.WebApi.Tests/Controllers/ValuesControllerTest.cs
+++ XYZCorp.WebApi.Tests/Controllers/ValuesControllerTest.cs
@@ -30,7 +30,7 @@ namespace XYZCorp.WebApi.Tests.Controllers
 
             _mockData.AddRange(new User[] {
                     new User { Id = 1, Name="Peter", Points = 1 },
-                    new User { Id = 1, Name="Jonh", Points = 2 }
+                    new User { Id = 2, Name="Jonh", Points = 2 }
                 });
         }
 
@@ -139,118 +139,54 @@ namespace XYZCorp.WebApi.Tests.Controllers
             }
         }
 
-        //////[Test]
-        //////public async Task Get_ById_FoundTypeOfUser()
-        //////{
-        //////    // Arrange
-        //////    //ValuesController controller = new ValuesController();
 
-        //////    // Act
-        //////    IHttpActionResult result = await _controller.Get(2);
-        //////    var contentResult = result as OkNegotiatedContentResult<User>;
+        [Test]
+        public void Get_ById_FoundId2()
+        {
+            //TODO
+            Assert.Pass();
+        }
 
-        //////    // Assert
-        //////    Assert.IsInstanceOf(typeof(User), contentResult.Content);
-        //////}
-
-        //////[Test]
-        //////public async Task Get_ById_FoundId2()
-        //////{
-        //////    // Arrange
-        //////    //ValuesController controller = new ValuesController();
-
-        //////    // Act
-        //////    IHttpActionResult result = await _controller.Get(2);
-        //////    var contentResult = result as OkNegotiatedContentResult<User>;
-
-        //////    // Assert
-        //////    Assert.NotNull(contentResult.Content);
-        //////    Assert.AreEqual(2, contentResult.Content.Id);
-        //////}
-
-        //////[Test]
-        //////public async Task Post_ValidRequestData_ReturnId()
-        //////{
-        //////    // Arrange
-        //////    //ValuesController controller = new ValuesController();
-
-        //////    var newUser = new User { Id = 3, Name = "Test", Points = 3 };
-
-        //////    // Act
-        //////    IHttpActionResult result =  await _controller.Post(newUser);
-        //////    var contentResult = result as OkNegotiatedContentResult<int>;
-
-        //////    // Assert
-        //////    Assert.AreEqual(newUser.Id, contentResult.Content);
-        //////}
+        [Test]
+        public void Post_ValidRequestData_ReturnId()
+        {
+            //TODO
+            Assert.Pass();
+        }
 
 
-        ///////// <summary>
-        /////////     will pass only if makes some changes in the model like making "Name" property as required field
-        ///////// </summary>
-        //////[Test]
-        //////public async Task Post_InvalidRequestData_ReturnBadRequestType()
-        //////{
-        //////    // Arrange
-        //////    //ValuesController controller = new ValuesController();
-        //////    var newUser = new User { Id = 3, Name="Test", Points = 3 };
+        /// <summary>
+        ///     will pass only if makes some changes in the model like making "Name" property as required field
+        /// </summary>
+        [Test]
+        public void Post_InvalidRequestData_ReturnBadRequestType()
+        {
+            //TODO
+            Assert.Pass();
+        }
 
-        //////    // Act
-        //////    IHttpActionResult result = await  _controller.Post(null); //newUser as parameter will make test fail
-        //////    var contentResult = result as InvalidModelStateResult;
+        [Test]
+        public void Put_ValidRequestData_ReturnSuccessMessage()
+        {
+            //TODO
+            Assert.Pass();
+        }
 
-        //////    // Assert
-        //////    Assert.IsInstanceOf<InvalidModelStateResult>(contentResult);
-        //////}
+        [Test]
+        public void Put_ValidRequestData_ReturnNotFoundType()
+        {
 
-        //////[Test]       
-        //////public async Task Put_ValidRequestData_ReturnSuccessMessage()
-        //////{
-        //////    // Arrange
-        //////    //ValuesController controller = new ValuesController();
-
-        //////    var existingUser = new User { Id = 2, Name = "Test", Points = 5 };
-
-        //////    // Act
-        //////    IHttpActionResult result = await _controller.Put(existingUser);
-        //////    var contentResult = result as OkNegotiatedContentResult<string>;
-
-        //////    // Assert
-        //////    Assert.AreEqual("Success", contentResult.Content);
-        //////}
-
-        //////[Test]
-        //////public async Task Put_ValidRequestData_ReturnNotFoundType()
-        //////{
-        //////    // Arrange
-        //////    //ValuesController controller = new ValuesController();
-
-        //////    var existingUser = new User { Id = -1, Name = "Test", Points = 5 };
-
-        //////    // Act
-        //////    IHttpActionResult result = await _controller.Put(existingUser);
-        //////    var contentResult = result as NotFoundResult;
-
-        //////    // Assert
-        //////    Assert.IsInstanceOf<NotFoundResult>(contentResult);
-        //////}
+        }
 
 
-        ///////// <summary>
-        /////////     will pass only if makes some changes in the model like making "Name" property as required field
-        ///////// </summary>
-        //////[Test]
-        //////public async Task Put_InvalidRequestData_ReturnBadRequestType()
-        //////{
-        //////    // Arrange
-        //////    //ValuesController controller = new ValuesController();
-
-        //////    // Act
-        //////    IHttpActionResult result = await _controller.Put(null);
-        //////    var contentResult = result as InvalidModelStateResult;
-
-        //////    // Assert
-        //////    Assert.IsInstanceOf<InvalidModelStateResult>(contentResult);
-        //////}
+        /// <summary>
+        ///     will pass only if makes some changes in the model like making "Name" property as required field
+        /// </summary>
+        [Test]
+        public void Put_InvalidRequestData_ReturnBadRequestType()
+        {
+            //TODO
+            Assert.Pass();
+        }
     }
 }
